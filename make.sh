908819086c5936864b761b757bc76c8110e6f6da
Maksim <pumpbro@gmail.com>
2024-02-29 19:37:30 +0300

    make.sh & extended reqs & workflow

---
+++ make.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -f ./src/Makefile ]; then
+    cd src
+fi
+
+if [ $(grep -i "\-Wall" ./Makefile 2>/dev/null | wc -l) -eq 0 ] || [ $(grep -i "\-Werror" ./Makefile 2>/dev/null | wc -l) -eq 0 ] || [ $(grep -i "\-Wextra" ./Makefile 2>/dev/null | wc -l) -eq 0 ]; then
+    echo "You forgot the gcc options -Wall -Werror and -Wextra"
+    exit 1
+fi 
+
+if [ $(grep -i "^ip_tools:" Makefile 2>/dev/null | wc -l) -eq 1 ]; then
+    make ip_tools
+fi
